7bcabf5f9b442daf4af3a1340af63c8faffa0473
Andrew Rob <andrew-c-rob@live.com>
2018-05-17 13:54:40 -0500

    Implemented more of the detectNetwork function

--- detectNetwork.js
+++ detectNetwork.js
@@ -6,7 +6,6 @@
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length) 
-var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
@@ -38,17 +37,23 @@ var detectNetwork = function(cardNumber) {
       //return 'Diner's Club'
     //else return 'Invalid'
 */
-
+var detectNetwork = function(cardNumber) {
   var prefix = cardNumber.substring(0, 2);
   var numOfDigits = cardNumber.length;
+  var cardNetwork = '';
 
   if((prefix === '34' || prefix === '37') && numOfDigits === 15) {
-    return 'American Express';
+    cardNetwork = 'American Express';
   } else if((prefix === '38' || prefix === '39') && numOfDigits === 14) {
-    return 'Diner\'s Club';
+    cardNetwork = 'Diner\'s Club';
+  } else if (cardNumber.substring(0, 1) === '4' && (numOfDigits === 13 || numOfDigits === 16 || numOfDigits === 19)) {
+    cardNetwork = 'Visa';
+  } else if  ((prefix === '51' || prefix === '52' || prefix === '53' || prefix === '54' || prefix === '55') && numOfDigits === 16) {
+    cardNetwork  = 'MasterCard';
   } else {
-    return 'Invalid';
+    cardNetwork = 'Invalid';
   }
+  return cardNetwork;
 };
 
   
