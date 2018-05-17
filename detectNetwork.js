675e6c6003f21fa661327a6fef32ba8c72070ef8
Andrew Rob <andrew-c-rob@live.com>
2018-05-17 13:17:46 -0500

    Implemented the first part of the function detectNetwork

--- detectNetwork.js
+++ detectNetwork.js
@@ -5,14 +5,50 @@
 // How can you tell one card network from another? Easy! 
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
-
+//   2. The number of digits in the number (called the length) 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  // Once you've read this, go ahead and try to implement this function, 
+  //then return to the console.
+  
+  /*
+  Inputs: string (ex: cardNumber)
+  Outputs: string (ex: 'Diner's Club or American Express')
+  Contraints: the cardNumber must be a string of numbers 
+  Edge: 1. no cardNumber inputted
+        2. an input that is not a string of numbers
+        3. invalid cardNumber length
+        4. an invalid prefix
+
+  Strategy/Transformations:
+    //cardNumber = '343456789012345' => 'American Express'
+    //We need to keep track of the prefix which is the first 2 numbers of 
+    input string
+    //We also need to keep track of the length of the cardNumber
+
+  Pseudocode:
+    //create a variable to store prefix
+    //create another variable to store cardNumber.length
+    //if prefix is 34 or 37 and cardNumber.length is 15 digits
+      //return 'American Express'
+    //else if prefix is 38 or 39 and cardNumber.length i 14 digits
+      //return 'Diner's Club'
+    //else return 'Invalid'
+*/
+
+  var prefix = cardNumber.substring(0, 2);
+  var numOfDigits = cardNumber.length;
+
+  if((prefix === '34' || prefix === '37') && numOfDigits === 15) {
+    return 'American Express';
+  } else if((prefix === '38' || prefix === '39') && numOfDigits === 14) {
+    return 'Diner\'s Club';
+  } else {
+    return 'Invalid';
+  }
 };
 
-
+  
